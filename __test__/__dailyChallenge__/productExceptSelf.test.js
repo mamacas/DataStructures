05971d1cada0c796c0ae815f342c7793bd4e268c
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-11-08 10:57:55 -0800

    solved product except self array challenge

---
+++ __test__/__dailyChallenge__/productExceptSelf.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const productExceptSelf = require('../../dailyChallenge/productExceptSelf.js');
+
+describe('Product of Array Except Self', () => {
+
+  it('should return a new array containing products of all array elements except itself', () => {
+
+    let nums = [1, 2, 3, 4];
+    expect(productExceptSelf(nums)).toEqual([24, 12, 8, 6]);
+  });
+
+});
+
